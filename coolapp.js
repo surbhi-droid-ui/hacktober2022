e7ff8f84704feaf4c000bd9fec0bca05dc61cef0
Rohit <34805019+Lonewolf22xx@users.noreply.github.com>
2022-10-18 20:05:38 +0530

    2nd

--- coolapp.js
+++ coolapp.js
@@ -1,5 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+<script>
+	const JSONToCSV = (objArray, keys) => {
+		let csv = keys.join(',');
+		objArray.forEach((row) => {
+			let values = [];
+			keys.forEach((key) => {
+				values.push(row[key] || '');
+			});
+			csv += '\n' + values.join(',');
+		});
+		return csv;
+	};
+
+	const exampleJSON = [
+		{
+			"date": 20210307,
+			"positives": 28756184,
+			"fatalities": 515148
+		},
+		{
+			"date": 20210306,
+			"positives": 28714654,
+			"fatalities": 514309
+		},
+		{
+			"date": 20210305,
+			"positives": 28654639,
+			"fatalities": 512629
+		},
+		{
+			"date": 20210304,
+			"positives": 28585852,
+			"fatalities": 510408
+		},
+		{
+			"date": 20210303,
+			"positives": 28520365,
+			"fatalities": 508665
+		},
+		{
+			"date": 20210302,
+			"positives": 28453529,
+			"fatalities": 506216
+		},
+		{
+			"date": 20210301,
+			"positives": 28399281,
+			"fatalities": 504488
+		}
+	];
+	console.log(JSONToCSV(exampleJSON,
+		['date', 'positives', 'fatalities']));
+</script>
 
 <head>
 	<meta charset="UTF-8">
